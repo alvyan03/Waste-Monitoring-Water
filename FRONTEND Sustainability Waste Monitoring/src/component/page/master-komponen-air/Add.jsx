abfe4e89292a525c65e72eab93193f82d2e34841
alvyan03 <alfianramdhan003@gmail.com>
2025-10-23 14:07:03 +0700

    ppppp

--- FRONTEND Sustainability Waste Monitoring/src/component/page/master-komponen-air/Add.jsx
+++ FRONTEND Sustainability Waste Monitoring/src/component/page/master-komponen-air/Add.jsx
@@ -83,6 +83,8 @@ export default function MasterKomponenAdd({ onChangePage }) {
   }, []);
 
   // Handler input
+  // MENGAMBIL DAFTAR LOKASI -- END
+
   const handleInputChange = async (e) => {
     const { name, value } = e.target;
     const validationError = await validateInput(name, value, userSchema);
@@ -154,6 +156,13 @@ export default function MasterKomponenAdd({ onChangePage }) {
         if (data === "ERROR") throw new Error("Gagal menyimpan data komponen.");
         SweetAlert("Sukses", "Data komponen berhasil disimpan", "success");
         onChangePage("index");
+
+        if (data === "ERROR") {
+          throw new Error("Terjadi kesalahan: Gagal menyimpan data komponen.");
+        } else {
+          SweetAlert("Sukses", "Data komponen berhasil disimpan", "success");
+          onChangePage("index");
+        }
       } catch (error) {
         setIsError({ error: true, message: error.message });
         setIsError({ error: true, message: error.message });
@@ -198,7 +207,7 @@ export default function MasterKomponenAdd({ onChangePage }) {
       <form onSubmit={handleAdd}>
         <div className="card">
           <div className="card-header bg-primary fw-medium text-white">
-            Tambah Data Komponen Baru
+            Tambah Data Komponen Baru vvvvvvvvvvvvvssssss
           </div>
           <div className="card-body p-4">
             <div className="row mb-3">
